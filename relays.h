39fd088e94665f0d4d8c42a5358608a5f30e1b4e
DelboyJay <dhymanjones@gmail.com>
2018-12-21 19:51:28 +0000

    fixed indents and reformatted file

--- relays.h
+++ relays.h
@@ -4,41 +4,41 @@
 
 #define MAX_RELAYS 3
 
-class Relays{
+class Relays {
 private:
-    const byte pins[MAX_RELAYS] = {3, 4, 5};
+  const byte pins[MAX_RELAYS] = {3, 4, 5};
   // default first relay as on for the pond pump
   byte states[MAX_RELAYS] = {1, 0, 0};
-    const char *names[MAX_RELAYS] = {
-    "Pond pump",
-    "LED Lights",
-    "Rockery Lights",
+  const char *names[MAX_RELAYS] = {
+          "Pond pump",
+          "LED Lights",
+          "Rockery Lights",
   };
-  
+
 public:
-  Relays(){
+  Relays() {
     // setup pins
-    for(int index=0;index<sizeof(pins);index++){
+    for (int index = 0; index < sizeof(pins); index++) {
       pinMode(pins[index], OUTPUT);
       this->set_state(index, this->states[index]);
     }
   };
 
-  void set_state(const int index, const bool value){
+  void set_state(const int index, const bool value) {
     // relays are active zero so flip the request
-    this->states[index] = (byte)value;
-    digitalWrite(pins[index], (int)!value);        
+    this->states[index] = (byte) value;
+    digitalWrite(pins[index], (int) !value);
   };
 
-  bool get_state(const int index){
+  bool get_state(const int index) {
     return this->states[index];
   };
 
-  const char* name(int index){
+  const char *name(int index) {
     return names[index];
   }
 
-  const int count(){
+  const int count() {
     return sizeof(pins);
   };
 };
