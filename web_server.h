a48fc9d809df95a9e5b3d33d433adb8dc3c9f5e3
DelboyJay <dhymanjones@gmail.com>
2018-12-21 19:52:17 +0000

    fixed indents and reformatted file

--- web_server.h
+++ web_server.h
@@ -6,152 +6,151 @@
 #define MAX_CALLBACKS 4
 
 // Define a callback function
-typedef void(*func_ptr_t)(EthernetClient& client, const char* request);
+typedef void(*func_ptr_t)(EthernetClient &client, const char *request);
 
-class WebServer{
+class WebServer {
 private:
-	IPAddress server_ip;
-	EthernetServer* server;
-	byte mac[6] = {0x8E, 0x19, 0xA7, 0x54, 0xD5, 0x97};
-	char client_buffer[CLIENT_BUF_SIZE + 1] = {0};
-	char registered_urls[MAX_CALLBACKS][33];
-	func_ptr_t	registered_callbacks[MAX_CALLBACKS];
-	int callback_counter = 0;
-	
+  IPAddress server_ip;
+  EthernetServer *server;
+  byte mac[6] = {0x8E, 0x19, 0xA7, 0x54, 0xD5, 0x97};
+  char client_buffer[CLIENT_BUF_SIZE + 1] = {0};
+  char registered_urls[MAX_CALLBACKS][33];
+  func_ptr_t registered_callbacks[MAX_CALLBACKS];
+  int callback_counter = 0;
+
 public:
-	WebServer(){
-		this->server_ip = IPAddress(192, 168, 1, 177);
-		this->server = new EthernetServer(80);
-	}
-	
-	~WebServer(){
-		delete this->server;
-	}
+  WebServer() {
+    this->server_ip = IPAddress(192, 168, 1, 177);
+    this->server = new EthernetServer(80);
+  }
 
-	//
-	// Initialise the Ethernet board
-	//
-	void init_board(){
-		// You can use Ethernet.init(pin) to configure the CS pin
-		Ethernet.init(10);  // Most Arduino shields
-		// start the Ethernet connection and the server:
-		Ethernet.begin(this->mac, this->server_ip);
-	}
+  ~WebServer() {
+    delete this->server;
+  }
 
-	//
-	// Bind the server to its port and listen for clients
-	//
-	void begin(){
-		this->server->begin();
-	}
-	
-	void get_server_ip_as_string(char buf[], bool add_port = true) {
-		get_ip_as_string(this->server_ip, buf, 80, add_port);
-	}
-	
-	static void get_client_ip_as_string(EthernetClient& client, char buf[]) {
-		get_ip_as_string(client.remoteIP(), buf, 0, false);
-	}
-	
-	static inline void get_ip_as_string(IPAddress ip, char buf[], int port, bool add_port = true){
-		if(add_port)
-			sprintf(buf, "%d.%d.%d.%d:%d", ip[0], ip[1], ip[2], ip[3], 80);
-		else
-			sprintf(buf, "%d.%d.%d.%d", ip[0], ip[1], ip[2], ip[3]);		
-	}
-	
-	EthernetClient available(){
-		return this->server->available();
-	}		
-		
-	//
-	// Processes one line at a time from the client request
-	//
-	bool get_request_line(EthernetClient& client) {
-		client_buffer[0] = '\0';
+  //
+  // Initialise the Ethernet board
+  //
+  void init_board() {
+    // You can use Ethernet.init(pin) to configure the CS pin
+    Ethernet.init(10);  // Most Arduino shields
+    // start the Ethernet connection and the server:
+    Ethernet.begin(this->mac, this->server_ip);
+  }
 
-		if (client.connected() && client.available()) {
-			for (int i = 0; i < CLIENT_BUF_SIZE; i++) {
-				char c = client.read();
-				// If there are no more characters then quit
-				if (c == -1)
-					return true;
+  //
+  // Bind the server to its port and listen for clients
+  //
+  void begin() {
+    this->server->begin();
+  }
 
-				client_buffer[i] = c;
-				if (c == '\n') {
-                    client_buffer[i + 1] = '\0';
-                    // If i<=1 then end of request detected
-                    return (i <= 1);
-				}
-			}
-		}
-		// No end of request detected
-		return false;
-	}
+  void get_server_ip_as_string(char buf[], bool add_port = true) {
+    get_ip_as_string(this->server_ip, buf, 80, add_port);
+  }
 
-	//
-	// Closes the client connection
-	//
-	void close_client_connection(EthernetClient& client) {
-		// give the web browser time to receive the data
-		delay(1);
-		// close the connection:
-		client.stop();
-		Serial.println("Client disconnected");
-	}
+  static void get_client_ip_as_string(EthernetClient &client, char buf[]) {
+    get_ip_as_string(client.remoteIP(), buf, 0, false);
+  }
 
-	//
-	// Process client request data
-	//
-	void process_client(EthernetClient& client, Relays& relays, const byte temps[2]) {
-	  while (!get_request_line(client)) {
-          Serial.println(client_buffer);
-			for(int i=0;i<callback_counter;i++)
-			{
-				if (!strncmp(client_buffer, registered_urls[i], strlen(registered_urls[i]))) {
-					while (client.read() != -1);
-					if(registered_callbacks[i]==0){
-						Serial.println("***ERROR*** - null callback");
-						return;
-					}
-					registered_callbacks[i](client, client_buffer);
-					return;
-				}
-			}
-		}
-	}
-	
-	//
-	// Sends a standard 200 OK client header.
-	//
-	static void send_client_header(
-			EthernetClient& client, 
-			int status_code=200, 
-			const char status_text[]="OK",
-			int refresh=0
-		) {
-		// send a standard http response header
-		client.print("HTTP/1.1 ");
-		client.print(status_code);
-		client.print(" ");
-		client.println(status_text);
-		client.println("Content-Type: text/html\nConnection: close");  
-		if (refresh) {
-			client.print("Refresh: ");
-			client.println(refresh);  // refresh the page automatically every n sec
-		}
-		client.println();
-	}
-		
-	void register_callback(const char* url_path, func_ptr_t callback){
-		if(callback_counter==MAX_CALLBACKS){
-			Serial.println("***ERROR*** - too many callbacks");
-			return;
-		}
-		
-		strcpy(registered_urls[callback_counter], url_path);
-		registered_callbacks[callback_counter] = callback;
-		callback_counter++;
-	}
-	
+  static inline void get_ip_as_string(IPAddress ip, char buf[], int port, bool add_port = true) {
+    if (add_port)
+      sprintf(buf, "%d.%d.%d.%d:%d", ip[0], ip[1], ip[2], ip[3], 80);
+    else
+      sprintf(buf, "%d.%d.%d.%d", ip[0], ip[1], ip[2], ip[3]);
+  }
+
+  EthernetClient available() {
+    return this->server->available();
+  }
+
+  //
+  // Processes one line at a time from the client request
+  //
+  bool get_request_line(EthernetClient &client) {
+    client_buffer[0] = '\0';
+
+    if (client.connected() && client.available()) {
+      for (int i = 0; i < CLIENT_BUF_SIZE; i++) {
+        char c = client.read();
+        // If there are no more characters then quit
+        if (c == -1)
+          return true;
+
+        client_buffer[i] = c;
+        if (c == '\n') {
+          client_buffer[i + 1] = '\0';
+          // If i<=1 then end of request detected
+          return (i <= 1);
+        }
+      }
+    }
+    // No end of request detected
+    return false;
+  }
+
+  //
+  // Closes the client connection
+  //
+  void close_client_connection(EthernetClient &client) {
+    // give the web browser time to receive the data
+    delay(1);
+    // close the connection:
+    client.stop();
+    Serial.println("Client disconnected");
+  }
+
+  //
+  // Process client request data
+  //
+  void process_client(EthernetClient &client, Relays &relays, const byte temps[2]) {
+    while (!get_request_line(client)) {
+      Serial.println(client_buffer);
+      for (int i = 0; i < callback_counter; i++) {
+        if (!strncmp(client_buffer, registered_urls[i], strlen(registered_urls[i]))) {
+          while (client.read() != -1);
+          if (registered_callbacks[i] == 0) {
+            Serial.println("***ERROR*** - null callback");
+            return;
+          }
+          registered_callbacks[i](client, client_buffer);
+          return;
+        }
+      }
+    }
+  }
+
+  //
+  // Sends a standard 200 OK client header.
+  //
+  static void send_client_header(
+          EthernetClient &client,
+          int status_code = 200,
+          const char status_text[] = "OK",
+          int refresh = 0
+  ) {
+    // send a standard http response header
+    client.print("HTTP/1.1 ");
+    client.print(status_code);
+    client.print(" ");
+    client.println(status_text);
+    client.println("Content-Type: text/html\nConnection: close");
+    if (refresh) {
+      client.print("Refresh: ");
+      client.println(refresh);  // refresh the page automatically every n sec
+    }
+    client.println();
+  }
+
+  void register_callback(const char *url_path, func_ptr_t callback) {
+    if (callback_counter == MAX_CALLBACKS) {
+      Serial.println("***ERROR*** - too many callbacks");
+      return;
+    }
+
+    strcpy(registered_urls[callback_counter], url_path);
+    registered_callbacks[callback_counter] = callback;
+    callback_counter++;
+  }
+
 };
